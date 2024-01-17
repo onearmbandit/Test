b8cbfba67f76511a72ad66132ff3c0f58a4adedf
Mayuri <mchaudhari@cemtrexlabs.com>
2024-01-17 23:48:54 +0530

    updated migrations as per new database schema

--- api/database/migrations/1705063811459_facility_products.ts
+++ api/database/migrations/1705063811459_facility_products.ts
@@ -5,19 +5,18 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.integer('facility_emission_id')
+      table.uuid('id').primary()
+      table.uuid('facility_emission_id')
       .nullable()
-      .unsigned()
       .references('facility_emissions.id')
       .onDelete('CASCADE')
       table.string('name')
       table.integer('quantity')
-      table.string('functional_unit')
+      table.string('functional_unit').comment('e.g., tCO2e, liters, kgs')
       table.integer('scope_1_carbon_emission')
       table.integer('scope_2_carbon_emission')
       table.integer('scope_3_carbon_emission')
-      table.tinyint('equality_attribute').defaultTo(0)
+      table.tinyint('equality_attribute').defaultTo(0).comment('0: not equal, 1: equal')
       table.timestamp('deleted_at', { useTz: true })
       table.timestamp('created_at', { useTz: true })
       table.timestamp('updated_at', { useTz: true })
--- api/database/migrations/1705064134298_suppliers.ts
+++ api/database/migrations/1705064134298_suppliers.ts
@@ -5,15 +5,14 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.integer('supply_chain_reporting_period_id')
+      table.uuid('id').primary()
+      table.uuid('supply_chain_reporting_period_id')
       .nullable()
-      .unsigned()
       .references('supply_chain_reporting_periods.id')
       .onDelete('CASCADE')
       table.string('name')
       table.string('email')
-      table.string('organization_relationship')
+      table.string('organization_relationship').comment('value must be either owned or contractor')
       table.string('address_line_1');
       table.string('address_line_2');
       table.string('city');
