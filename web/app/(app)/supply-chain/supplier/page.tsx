2e8488d08c4aafcb765b5d115d2c1580d9b21697
Chaitanya K <ckanzarkar@cxr.agency>
2024-02-05 17:23:28 +0530

    feat: service file updated for supplier

---
+++ web/app/(app)/supply-chain/supplier/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import AddSupplierManualy from "@/components/supply-chain/addSupplierManualy";
+import React from "react";
+
+const page = () => {
+  return (
+    <>
+      <AddSupplierManualy />
+    </>
+  );
+};
+
+export default page;
--- web/services/supply.chain.ts
+++ web/services/supply.chain.ts
@@ -1,34 +1,34 @@
-'use server';
+"use server";
 import axios, {
   AxiosHeaders,
   AxiosRequestConfig,
   RawAxiosRequestHeaders,
-} from 'axios';
+} from "axios";
 
-import { authOptions } from '@/lib/utils';
-import { getServerSession } from 'next-auth';
+import { authOptions } from "@/lib/utils";
+import { getServerSession } from "next-auth";
 type CreateAPIMethod = <TInput extends Record<string, string>, TOutput>(opts: {
   url: string;
-  method: 'GET' | 'POST' | 'PATCH' | 'PUT';
+  method: "GET" | "POST" | "PATCH" | "PUT";
 }) => (input: TInput) => Promise<TOutput>;
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 type Options = {
-  method?: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
+  method?: "GET" | "POST" | "PATCH" | "PUT" | "DELETE";
   body?: any;
   headers?: AxiosHeaders;
 };
 
 const fetchApi = async (
   url: string,
-  options = { method: 'GET' } as Options
+  options = { method: "GET" } as Options
 ) => {
   const { headers, method, body } = options;
   // const session = await getServerSession(authOptions);
   const session = await getServerSession(authOptions);
   const token = session?.token.token;
-  console.log(token, 'token');
+  console.log(token, "token");
   const authHeader = {
     Authorization: `bearer ${token}`,
   };
@@ -40,7 +40,7 @@ const fetchApi = async (
   })
     .then((res) => res.data)
     .catch((err) => {
-      console.log(err.response.data, 'error');
+      console.log(err.response.data, "error");
       throw new Error(err.response.data.message);
     });
 
@@ -48,20 +48,20 @@ const fetchApi = async (
 };
 export const importFile = (data: any) => {
   const formbody = data;
-  return fetchApi('/auth/supplier-csv-upload', {
-    method: 'POST',
+  return fetchApi("/auth/supplier-csv-upload", {
+    method: "POST",
     body: formbody,
   });
 };
 export const addReportingPeriod = (data: any) => {
   const formbody = data;
-  return fetchApi('/auth/supplier-period', {
-    method: 'POST',
+  return fetchApi("/auth/supplier-period", {
+    method: "POST",
     body: formbody,
   });
 };
 export const getAllReportingPeriods = (organizationId: string) => {
-  console.log(organizationId, 'organizationId');
+  console.log(organizationId, "organizationId");
   return fetchApi(
     `/auth/supplier-period?page=1&per_page=10&organizationId=${organizationId}`
   );
@@ -69,17 +69,35 @@ export const getAllReportingPeriods = (organizationId: string) => {
 export const editReportingPerid = ({ id, formData }: any) => {
   const formbody = formData;
   return fetchApi(`/auth/supplier-period/${id}`, {
-    method: 'PATCH',
+    method: "PATCH",
     body: formbody,
   });
 };
 export const deleteReportingPerid = (id: string) => {
   return fetchApi(`/auth/supplier-period/${id}`, {
-    method: 'DELETE',
+    method: "DELETE",
   });
 };
 export const downloadCsvTemplate = () => {
-  return fetchApi('/download-supplier-csv', {
-    method: 'GET',
+  return fetchApi("/download-supplier-csv", {
+    method: "GET",
+  });
+};
+
+// add supplier manually
+export const addSupplier = (data: any) => {
+  const formbody = data;
+  return fetchApi("/auth/suppliers", {
+    method: "POST",
+    body: formbody,
+  });
+};
+
+//update supplier
+export const updateSupplier = (data: any) => {
+  const formbody = data;
+  return fetchApi(`/auth/suppliers/${data.id}`, {
+    method: "PATCH",
+    body: formbody,
   });
 };
