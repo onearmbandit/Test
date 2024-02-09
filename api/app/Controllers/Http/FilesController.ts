4c1682c1e71ae16e605efb2197f567fb2aa5643e
Mayuri <mchaudhari@cemtrexlabs.com>
2024-02-09 19:46:30 +0530

    commented unused s3 uplaod code

--- api/app/Controllers/Http/FilesController.ts
+++ api/app/Controllers/Http/FilesController.ts
@@ -32,74 +32,74 @@ export default class FilesController {
 
 
   //:: Upload image to s3 bucket
-  public async uploadImageToS31({ request, response }: HttpContextContract) {
-    try {
+  // public async uploadImageToS31({ request, response }: HttpContextContract) {
+  //   try {
 
-      var payload = await request.validate(ImageUploadValidator);
+  //     var payload = await request.validate(ImageUploadValidator);
 
-      // * File upload using aws-sdk library
-      const bucket = Env.get('AWS_BUCKET')
-      const accessKeyId = Env.get('AWS_ACCESS_KEY_ID')
-      const secretAccessKey = Env.get('AWS_SECRET_ACCESS_KEY')
-      const region = Env.get('AWS_DEFAULT_REGION')
-      if (bucket && accessKeyId && secretAccessKey && payload.image.tmpPath) {
-        // Set your AWS credentials and region
-        const s3Client = new S3Client({
-          region:
-            Env.get('AWS_DEFAULT_REGION'),
-          credentials: {
-            accessKeyId: accessKeyId,
-            secretAccessKey: secretAccessKey,
-          },
-        })
+  //     // * File upload using aws-sdk library
+  //     const bucket = Env.get('AWS_BUCKET')
+  //     const accessKeyId = Env.get('AWS_ACCESS_KEY_ID')
+  //     const secretAccessKey = Env.get('AWS_SECRET_ACCESS_KEY')
+  //     const region = Env.get('AWS_DEFAULT_REGION')
+  //     if (bucket && accessKeyId && secretAccessKey && payload.image.tmpPath) {
+  //       // Set your AWS credentials and region
+  //       const s3Client = new S3Client({
+  //         region:
+  //           Env.get('AWS_DEFAULT_REGION'),
+  //         credentials: {
+  //           accessKeyId: accessKeyId,
+  //           secretAccessKey: secretAccessKey,
+  //         },
+  //       })
 
-        const uploadedImage = {
-          Bucket: bucket,
-          Key: payload.image.clientName,
-          Body: fs.readFileSync(payload.image.tmpPath),
-          Metadata: {
-            'Content-Disposition': 'inline',
-          }
-        }
+  //       const uploadedImage = {
+  //         Bucket: bucket,
+  //         Key: payload.image.clientName,
+  //         Body: fs.readFileSync(payload.image.tmpPath),
+  //         Metadata: {
+  //           'Content-Disposition': 'inline',
+  //         }
+  //       }
 
-        const command = new PutObjectCommand(uploadedImage)
-        const result = await s3Client.send(command)
+  //       const command = new PutObjectCommand(uploadedImage)
+  //       const result = await s3Client.send(command)
 
-        if (result.$metadata.httpStatusCode === 200) {
+  //       if (result.$metadata.httpStatusCode === 200) {
 
-          // const ImageUrl = Env.get('AWS_ENDPOINT') + '/' + uploadedImage.Key
+  //         // const ImageUrl = Env.get('AWS_ENDPOINT') + '/' + uploadedImage.Key
 
-          const ImageUrl = `https://${bucket}.s3.${region}.amazonaws.com/${uploadedImage.Key}`;
+  //         const ImageUrl = `https://${bucket}.s3.${region}.amazonaws.com/${uploadedImage.Key}`;
 
-          //store url into database
-          return apiResponse(response, true, 201, { 'url': ImageUrl },
-            Config.get('responsemessage.FILE_UPLOAD_RESPONSE.uploadSuccess'));
-        }
-      } else {
-        return apiResponse(response, false, 400, {}, Config.get('responsemessage.FILE_UPLOAD_RESPONSE.serverError'))
-      }
+  //         //store url into database
+  //         return apiResponse(response, true, 201, { 'url': ImageUrl },
+  //           Config.get('responsemessage.FILE_UPLOAD_RESPONSE.uploadSuccess'));
+  //       }
+  //     } else {
+  //       return apiResponse(response, false, 400, {}, Config.get('responsemessage.FILE_UPLOAD_RESPONSE.serverError'))
+  //     }
 
-    } catch (error) {
-      console.log("error", error)
-      if (error.status === 422) {
-        return apiResponse(
-          response,
-          false,
-          error.status,
-          error.messages,
-          Config.get('responsemessage.COMMON_RESPONSE.validationFailed')
-        )
-      } else {
-        return apiResponse(
-          response,
-          false,
-          400,
-          {},
-          error.messages ? error.messages : error.message
-        )
-      }
-    }
-  }
+  //   } catch (error) {
+  //     console.log("error", error)
+  //     if (error.status === 422) {
+  //       return apiResponse(
+  //         response,
+  //         false,
+  //         error.status,
+  //         error.messages,
+  //         Config.get('responsemessage.COMMON_RESPONSE.validationFailed')
+  //       )
+  //     } else {
+  //       return apiResponse(
+  //         response,
+  //         false,
+  //         400,
+  //         {},
+  //         error.messages ? error.messages : error.message
+  //       )
+  //     }
+  //   }
+  // }
 
   // Upload image in s3 bucket
   public async uploadImageToS3({ request, response }: HttpContextContract) {
