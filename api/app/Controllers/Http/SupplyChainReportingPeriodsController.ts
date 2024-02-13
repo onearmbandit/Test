48f080f7fcca858fedcaf3d2d502cd8b3a94a5d8
anjpawar-cemtrex <anjpawar@cemtrexlabs.com>
2024-02-13 17:28:59 +0530

    Supply chain-> Handled organizationId field in create and get all reporting period API.

--- api/app/Controllers/Http/SupplyChainReportingPeriodsController.ts
+++ api/app/Controllers/Http/SupplyChainReportingPeriodsController.ts
@@ -3,17 +3,30 @@ import { apiResponse } from 'App/helpers/response'
 import Config from '@ioc:Adonis/Core/Config';
 import SupplyChainReportingPeriodValidator from 'App/Validators/Supplier/SupplyChainReportingPeriodValidator';
 import SupplyChainReportingPeriod from 'App/Models/SupplyChainReportingPeriod';
-
+import User from 'App/Models/User';
 
 export default class SupplyChainReportingPeriodsController {
-  public async index({ request, response }: HttpContextContract) {
+  public async index({ request, response,auth }: HttpContextContract) {
     try {
 
       const queryParams = request.qs();
 
+      //:: Check organization id is same for auth user or not
+      const userFound = await User.getUserDetails('id', auth.user?.id)
+      let organizationIds = (await userFound.organizations).map((item) => item.id)
+      if (queryParams.organizationId && !organizationIds.includes(queryParams.organizationId)) {
+        return apiResponse(
+          response,
+          false,
+          403,
+          {},
+          "The provided organization ID does not belongs to you."
+        )
+      }
+
       const reportingPeriods = await SupplyChainReportingPeriod.getAllReportingPeriod(queryParams);
 
-      const isPaginated = !request.input('per_page') || request.input('per_page') !== 'all';
+      const isPaginated = request.input('per_page') && request.input('per_page') !== 'all';
 
       return apiResponse(response, true, 200, reportingPeriods, Config.get('responsemessage.COMMON_RESPONSE.getRequestSuccess'), isPaginated);
 
@@ -47,7 +60,21 @@ export default class SupplyChainReportingPeriodsController {
       // validate facility details
       await request.validate(SupplyChainReportingPeriodValidator)
 
-      const reportPeriodData = await SupplyChainReportingPeriod.createReportPeriod(requestData, auth)
+
+      //:: Check organization id is same for auth user or not
+      const userFound = await User.getUserDetails('id', auth.user?.id)
+      let organizationIds = (await userFound.organizations).map((item) => item.id)
+      if (requestData.organizationId && !organizationIds.includes(requestData.organizationId)) {
+        return apiResponse(
+          response,
+          false,
+          403,
+          {},
+          "The provided organization ID does not belongs to you."
+        )
+      }
+
+      const reportPeriodData = await SupplyChainReportingPeriod.createReportPeriod(requestData, organizationIds[0])
 
 
 
